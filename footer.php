3f6502d3a0548dc1ee86528f2526fc0bc8f19676
mattJamesMedia <mattjamesmedia@gmail.com>
2018-04-10 17:54:16 -0500

    add widgets in footer, remove clearfix and floats for nav

--- footer.php
+++ footer.php
@@ -25,6 +25,30 @@
 				printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="https://automattic.com/">Automattic</a>' );
 			?>
 		</div><!-- .site-info -->
+
+<div id="footer-sidebar" class="secondary">
+<div id="footer-sidebar1">
+<?php
+if(is_active_sidebar('footer-sidebar-1')){
+dynamic_sidebar('footer-sidebar-1');
+}
+?>
+</div>
+<div id="footer-sidebar2">
+<?php
+if(is_active_sidebar('footer-sidebar-2')){
+dynamic_sidebar('footer-sidebar-2');
+}
+?>
+</div>
+<div id="footer-sidebar3">
+<?php
+if(is_active_sidebar('footer-sidebar-3')){
+dynamic_sidebar('footer-sidebar-3');
+}
+?>
+</div>
+</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
