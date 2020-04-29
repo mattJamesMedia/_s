415d218fbda183a718d9d5d7195cce4d61231fc8
Matt James <matt.james@secondstreet.com>
2020-04-29 14:07:33 -0500

    add custom JS option

--- functions.php
+++ functions.php
@@ -149,7 +149,14 @@ function _s_scripts() {
 	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
+	/**
+	 * Uncomment below function if you want to write custom JS
+	 */
+	// wp_enqueue_script( '_s-script', get_template_directory_uri() . '/js/script.js', array(), '20200429', true );
+	/**
+	 * Uncomment below function if you want to write custom JS and use jQuery
+	 */
+	// wp_enqueue_script( '_s-script', get_template_directory_uri() . '/js/script.js', array('jquery), '20200429', true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
